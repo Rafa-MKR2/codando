81901648d7b27fa4e111e2e41f5ba236af1129ad
Rafa-MKR2 <rafaeldocarmo2010@gmail.com>
2019-02-23 18:27:19 -0300

    Correçoes, ajustes e otimizaçao de codigo

--- js/style.js
+++ js/style.js
@@ -1,16 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelector('.carousel');
-    var instances = M.Carousel.init(elems, 5);
+     M.Carousel.init(elems, 5);
   });
 
 
 
 
+  function myFunction() {
+    let event = window.scrollY
+  if(event>120) {
+    document.querySelector('header').classList.add("ativar");
 
-  document.getElementById("menu1").addEventListener('click', function(){butEvent = 1;topFunction();});
-document.getElementById("menu2").addEventListener('click', function(){butEvent = 2; topFunction();})
-document.getElementById("menu3").addEventListener('click', function(){butEvent = 3; topFunction();})
-var butEvent = 0;
+
+ }else{
+  document.querySelector('header').classList.remove("ativar")
+
+ }
+
+}
+
+document.getElementById("menu1").addEventListener('click', function(){scrollEvent = 1;topFunction();});
+document.getElementById("menu2").addEventListener('click', function(){scrollEvent = 2; topFunction();})
+document.getElementById("menu3").addEventListener('click', function(){scrollEvent = 3; topFunction();})
+document.getElementById("orcament-btn").addEventListener('click', function(){scrollEvent = 3; topFunction();})
+
+var scrollEvent = 0;
 var servico = document.getElementById("titulo-servico");
 var portifolio = document.getElementById("portifolio");
 var orcamento = document.getElementById("orcamento");
@@ -22,17 +36,23 @@ let width = window.innerWidth ||
         document.body.clientWidth;
 
 if(width<760) screenWidth=95
-        if(butEvent == 1){
 
-      window.scroll({top: servico.offsetTop-screenWidth, behavior: 'smooth'});
+switch(scrollEvent){
+  case 1:
+  window.scroll({top: servico.offsetTop-screenWidth, behavior: 'smooth'});
+  break;
 
-    }
-    if(butEvent == 2){    
-      window.scroll({top: portifolio.offsetTop-screenWidth, behavior: 'smooth'});
+  case 2:
+     window.scroll({top: portifolio.offsetTop-screenWidth, behavior: 'smooth'});
+  break;
 
+  case 3:
+  window.scroll({top: orcamento.offsetTop-screenWidth, behavior: 'smooth'});
+  break;
 
-	}
-    if(butEvent == 3){
-        window.scroll({top: orcamento.offsetTop-screenWidth, behavior: 'smooth'});
-	}
+  case false:
+  window.scrollTo(0,0);
+  break;
+}
+
 }
