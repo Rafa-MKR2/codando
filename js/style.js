a60f6d2c23f132684bbf2b53d09a59725ea86412
DANIEL dias <danielalce4@gmail.com>
2019-02-23 19:22:02 -0300

    melhorando o cabeçalho e colocando o curso pointer nos itens do menu e mudando a frase da intro no mobile

--- js/style.js
+++ js/style.js
@@ -10,11 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
     let event = window.scrollY
   if(event>120) {
     document.querySelector('header').classList.add("ativar");
+    document.querySelector('.logo').classList.add("logoM");  
 
-
+   document.querySelector('.menu').classList.add("menuM");
+      
  }else{
   document.querySelector('header').classList.remove("ativar")
+   document.querySelector('.logo').classList.remove("logoM");  
 
+   document.querySelector('.menu').classList.remove("menuM");
  }
 
 }
